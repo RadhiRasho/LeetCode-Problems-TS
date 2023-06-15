aff4327b55a08b81933020e53f621af256aa9feb
Radhi B. Rasho <Radhi.h@outlook.com>
2023-06-15 05:57:18 +0000

    testing...

--- leetcode/plus-one.ts
+++ leetcode/plus-one.ts
@@ -1,7 +1,7 @@
 export function plusOne(digits: number[]) {
-  let sum = 0;
-  for (let i = 0; i < digits.length; ++i) {
-    sum += digits[i];
-  }
-  return sum;
+	let sum = 0;
+	for (let i = 0; i < digits.length; ++i) {
+		sum += digits[i];
+	}
+	return sum;
 }
